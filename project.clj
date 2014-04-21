0688e026b34b415bdc8a8fdbb99c8905344fe3b6
Alf Kristian Stoyle <alf.kristian@gmail.com>
2014-04-21 16:12:03 +0200

    Updated licencse.

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
 (defproject clojure-workshop-flatmap "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description
+  "Workshop material for learning clojure.
+  lein uberjar in solution branch will create the tac program-"
+  :url "https://gitlab.com/stoyle/clojure-workshop-flatmap"
+  :license {:name "Creative Commons Attribution 4.0 International Public License"
+            :url "http://creativecommons.org/licenses/by/4.0/"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.cli "0.3.1"]]
   :main clojure-workshop-flatmap.tac
