db139aea605306c577aa0ddda2d5fb537612e1e9
Alf Kristian Stoyle <alf.kristian@gmail.com>
2014-05-02 13:15:13 +0200

    Two flags now working, also with tests.

--- src/clojure_workshop_flatmap/cat.clj
+++ src/clojure_workshop_flatmap/cat.clj
@@ -5,24 +5,30 @@
       [clojure-workshop-flatmap.args :as args]
       [clojure.string :as str]))
 
-(defn read-file [f]
-  (slurp f))
+(defn split-retain-empty-lines
+  "Splits a line into a seq, where empty lines are retaines as an empty string \"\""
+  [text]
+  (str/split text #"\r?\n"))
 
-(defn format-line [line num]
+(defn format-line
+  "Formats lines with prefixed numbering"
+  [line num]
   (format "%6d %s" num line))
 
-(defn number-lines [state text]
-  (let [current-cnt (:line-cnt state 1)
-        lines (str/split-lines text)
-        formatted-lines (map format-line lines
-                             (iterate inc current-cnt))
-        cnt (+ current-cnt (count lines))]
-    [(assoc state :line-cnt cnt)
-     (str/join \newline formatted-lines)]))
+(declare number-lines number-non-blank-lines read-file)
 
-(defn number-non-blank-lines [state text]
-  (let [lines (str/split-lines text)]
-    [state text]))
+;; Check out tests in clojure-workshop-flatmap.cat-test, comment them in one by one, which should
+;; Help you build your cat program.
+;; You should implement the functions starting with (in increasing difficulty)
+;; * read-file
+;; * cat
+;; * number-lines
+;; * number-non-blank-lines
+
+(defn read-file
+  "Reads a file from disk. What is the easiest way? (Opposite of spit)"
+  [f]
+  (slurp f))
 
 (defn cat
   "Takes state (opts) and text to be formatted as params. Returns a vector of current state and text
@@ -36,6 +42,36 @@
     (:num-all-lines state) (number-lines state text)
     :else [state text]))
 
+
+
+(defn number-lines
+  "Takes state (potentially containg a current :line-count) and text  and formats it using format
+  line. Where each line is prefixed with a its number starting with 1. Returns a vector containing
+  a new state with a new :line-cnt, and the formatted-lines."
+  [state text]
+  (let [lines (split-retain-empty-lines text)
+        current-cnt (:line-cnt state 1)
+        formatted-lines (map format-line lines
+                             (iterate inc current-cnt))
+        cnt (+ current-cnt (count lines))]
+    [(assoc state :line-cnt cnt)
+     (str/join \newline formatted-lines)]))
+
+(defn number-non-blank-lines
+  "Takes state (potentially containg a current :line-count) and text and formats it using format
+  line. If a line is non-empty it will be prefixed with its number starting with 1. Returns a
+  vector containing a new state with a new :line-cnt, and the formatted-lines."
+  [state text]
+  (let [current-cnt (:line-cnt state 1)
+        lines (split-retain-empty-lines text)
+        [cnt formatted-lines] (reduce (fn [[n xs] x]
+                                        (if-not (= "" x)
+                                          [(inc n) (conj xs (format-line x n))]
+                                          [n (conj xs "")]))
+                                      [current-cnt []] lines)]
+    [(assoc state :line-cnt cnt)
+     (str/join \newline formatted-lines)]))
+
 (defn cat-files
   "Takes options and a set of filenames to handle. First reads each file to text, then converts text
   for each file over cat. Then converts each result vector to only the text (the second part of cat
@@ -46,7 +82,9 @@
          (map (partial cat opts)
               (map read-file files)))))
 
-(defn cat-in [opts]
+(defn cat-in
+  "Loops over system/in until ctrl-d is pressed converting input to cat for each new line"
+  [opts]
   (loop [[state text] [opts nil]]
     (when text (println text))
     (when-let [line (read-line)]
--- test/clojure_workshop_flatmap/cat_test.clj
+++ test/clojure_workshop_flatmap/cat_test.clj
@@ -3,13 +3,40 @@
         clojure-workshop-flatmap.cat)
   (:require [clojure.string :as str]))
 
+(fact "read-file should actually read file contets"
+      (read-file "project.clj") => not-empty)
+
+(fact "cat should return its input and state in a vector"
+      (cat {} "1\n2") => [{} "1\n2"]
+      (cat {:state 1} "1\n2") => [{:state 1} "1\n2"])
+
 (fact "Should number all lines"
       (number-lines {} "text") =>   (contains      "     1 text")
       (number-lines {} "en\nto") => (contains (str "     1 en\n"
-                                                   "     2 to")))
+                                                   "     2 to"))
+      (number-lines {} "en\n\nto") => (contains (str "     1 en\n"
+                                                     "     2 \n"
+                                                     "     3 to")))
 
 (fact "Should number all lines and keep record of current line numbering"
-      (number-lines {} "text") =>   [{:line-cnt 2}               "     1 text"]
-      (number-lines {:line-cnt 2} "text") => [{:line-cnt 3}      "     2 text"]
-      (number-lines {} "en\nto") => [{:line-cnt 3} (str          "     1 en\n"
-                                                                 "     2 to")])
+      (number-lines {} "text") =>   [{:line-cnt 2}          "     1 text"]
+      (number-lines {:line-cnt 2} "text") => [{:line-cnt 3} "     2 text"]
+      (number-lines {} "en\nto") => [{:line-cnt 3} (str     "     1 en\n"
+                                                            "     2 to")])
+
+(fact "Should number non-empty-lines"
+      (number-non-blank-lines {} "text") =>   (contains        "     1 text")
+      (number-non-blank-lines {} "en\nto") => (contains   (str "     1 en\n"
+                                                               "     2 to"))
+      (number-non-blank-lines {} "en\n\nto") => (contains (str "     1 en\n"
+                                                               "\n"
+                                                               "     2 to")))
+
+(fact "Should number all lines and keep record of current line numbering"
+      (number-non-blank-lines {} "text") =>   [{:line-cnt 2}                 "     1 text"]
+      (number-non-blank-lines {:line-cnt 2} "text") => [{:line-cnt 3}        "     2 text"]
+      (number-non-blank-lines {} "en\nto") => [{:line-cnt 3} (str            "     1 en\n"
+                                                                             "     2 to")]
+      (number-non-blank-lines {:line-cnt 2} "en\n\nto") => [{:line-cnt 4} (str "     2 en\n"
+                                                                             "\n"
+                                                                             "     3 to")])
